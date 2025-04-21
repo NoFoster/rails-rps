0e52022c2736a2dfad320e2e6a844a6fa6633a6b
NoFoster <kfoster0@chicagobooth.edu>
2025-04-21 16:52:06 +0000

    basic_structure

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -3,6 +3,48 @@ class ZebraController < ApplicationController
 
     @random_move = ["rock", "paper", "scissors"].sample
 
+    if @random_move == "rock"
+      @outcome = "tied"
+    elsif @random_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
     render ({ :template => "games_templates/play_rock" })
   end
+
+  def elephant
+
+    @random_move = ["rock", "paper", "scissors"].sample
+
+    if @random_move == "rock"
+      @outcome = "won"
+    elsif @random_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
+
+    render ({ :template => "games_templates/play_paper"})
+  end
+
+  def lion
+    @random_move = ["rock", "paper", "scissors"].sample
+
+    if @random_move == "rock"
+      @outcome = "lost"
+    elsif @random_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
+
+    render ({ :template => "games_templates/play_scissors"})
+  end
+
+  def rules
+    render ({ :template => "games_templates/rules"})
+  end
+  
 end
